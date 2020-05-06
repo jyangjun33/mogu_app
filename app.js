5e97989f1e48f916da798831bbfc6c5c975dd472
jyangjun <592873224@qq.com>
2020-05-06 19:13:05 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//app.js
+App
+(
+  {
+  onLaunch: function () 
+    {
+      //云开发环境初始化
+      wx.cloud.init
+      (
+        {
+          env:"jyangjun-9oq0x",
+        }
+      )
+    }
+  }
+)
---
+++ pages/index/index.js
@@ -0,0 +1,14 @@
+//index.js
+//获取应用实例
+Page
+(
+  {
+    
+  }
+)
+ wx.cloud.callFunction({
+  name:"test",
+  // complete:console.log
+ })
+
+// console.log('hello world!')
